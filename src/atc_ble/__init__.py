5cf8d829f35a070436e39c7df4ef8cbd07b098e3
Ernst Klamer <e.klamer@gmail.com>
2022-08-14 21:38:54 +0200

    feat: initial commit

---
+++ src/atc_ble/__init__.py
@@ -0,0 +1,28 @@
+"""Parser for BLE advertisements for sensors with ATC firmware."""
+from __future__ import annotations
+
+from sensor_state_data import (
+    DeviceClass,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .parser import ATCBluetoothDeviceData
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "ATCBluetoothDeviceData",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "DeviceClass",
+    "DeviceKey",
+    "SensorUpdate",
+    "SensorDeviceInfo",
+    "SensorValue",
+    "Units",
+]
---
+++ tests/test_parser.py
@@ -0,0 +1,292 @@
+import logging
+from unittest.mock import patch
+
+import pytest
+from bluetooth_sensor_state_data import BluetoothServiceInfo, DeviceClass, SensorUpdate
+from sensor_state_data import (
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceInfo,
+    SensorValue,
+    Units,
+)
+
+from atc_ble.parser import ATCBluetoothDeviceData
+
+KEY_TEMPERATURE = DeviceKey(key="temperature", device_id=None)
+KEY_HUMIDITY = DeviceKey(key="humidity", device_id=None)
+KEY_BATTERY = DeviceKey(key="battery", device_id=None)
+KEY_VOLTAGE = DeviceKey(key="voltage", device_id=None)
+KEY_SIGNAL_STRENGTH = DeviceKey(key="signal_strength", device_id=None)
+
+
+@pytest.fixture(autouse=True)
+def logging_config(caplog):
+    caplog.set_level(logging.DEBUG)
+
+
+@pytest.fixture(autouse=True)
+def mock_platform():
+    with patch("sys.platform") as p:
+        p.return_value = "linux"
+        yield p
+
+
+def bytes_to_service_info(
+    payload: bytes, local_name: str, address: str = "00:00:00:00:00:00"
+) -> BluetoothServiceInfo:
+    return BluetoothServiceInfo(
+        name=local_name,
+        address=address,
+        rssi=-60,
+        manufacturer_data={},
+        service_data={"0000181a-0000-1000-8000-00805f9b34fb": payload},
+        service_uuids=["0000181a-0000-1000-8000-00805f9b34fb"],
+        source="",
+    )
+
+
+def test_can_create():
+    ATCBluetoothDeviceData()
+
+
+def test_atc1441_no_encryption(caplog):
+    """Test ATC parser for LYWSD03MMC with atc1441 firmware without encryption."""
+    data_string = b'\xa4\xc18\x8d\x18\xb2\x01\x12/d\x0c\xa0%'
+    advertisement = bytes_to_service_info(data_string, local_name="ATC_8D18B2", address="A4:C1:38:8D:18:B2")
+
+    device = ATCBluetoothDeviceData()
+    assert device.update(advertisement) == SensorUpdate(
+        title="ATC_8D18B2 (A4:C1:38:8D:18:B2)",
+        devices={
+            None: SensorDeviceInfo(
+                name="ATC_8D18B2 (A4:C1:38:8D:18:B2)",
+                manufacturer="ATC",
+                model="ATC sensor",
+                sw_version="ATC (atc1441)",
+                hw_version=None,
+            )
+        },
+        entity_descriptions={
+            KEY_TEMPERATURE: SensorDescription(
+                device_key=KEY_TEMPERATURE,
+                device_class=DeviceClass.TEMPERATURE,
+                native_unit_of_measurement=Units.TEMP_CELSIUS,
+            ),
+            KEY_HUMIDITY: SensorDescription(
+                device_key=KEY_HUMIDITY,
+                device_class=DeviceClass.HUMIDITY,
+                native_unit_of_measurement=Units.PERCENTAGE,
+            ),
+            KEY_BATTERY: SensorDescription(
+                device_key=KEY_BATTERY,
+                device_class=DeviceClass.BATTERY,
+                native_unit_of_measurement=Units.PERCENTAGE,
+            ),
+            KEY_VOLTAGE: SensorDescription(
+                device_key=KEY_VOLTAGE,
+                device_class=DeviceClass.VOLTAGE,
+                native_unit_of_measurement=Units.ELECTRIC_POTENTIAL_VOLT,
+            ),
+            KEY_SIGNAL_STRENGTH: SensorDescription(
+                device_key=KEY_SIGNAL_STRENGTH,
+                device_class=DeviceClass.SIGNAL_STRENGTH,
+                native_unit_of_measurement=Units.SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+            ),
+        },
+        entity_values={
+            KEY_TEMPERATURE: SensorValue(
+                device_key=KEY_TEMPERATURE, name="Temperature", native_value=27.4
+            ),
+            KEY_HUMIDITY: SensorValue(
+                device_key=KEY_HUMIDITY, name="Humidity", native_value=47
+            ),
+            KEY_BATTERY: SensorValue(
+                device_key=KEY_BATTERY, name="Battery", native_value=100
+            ),
+            KEY_VOLTAGE: SensorValue(
+                device_key=KEY_VOLTAGE, name="Voltage", native_value=3.232
+            ),
+            KEY_SIGNAL_STRENGTH: SensorValue(
+                name="Signal Strength", device_key=KEY_SIGNAL_STRENGTH, native_value=-60
+            ),
+        },
+    )
+
+
+def test_atc_pvvx_no_encryption(caplog):
+    """Test ATC parser for LYWSD03MMC with pvvx custom firmware without encryption."""
+    data_string = b'\xb2\x18\x8d8\xc1\xa4Y\n\xad\x13\xb6\t\x1f\x1e\x05'
+    advertisement = bytes_to_service_info(data_string, local_name="ATC_8D18B2", address="A4:C1:38:8D:18:B2")
+
+    device = ATCBluetoothDeviceData()
+    assert device.update(advertisement) == SensorUpdate(
+        title="ATC_8D18B2 (A4:C1:38:8D:18:B2)",
+        devices={
+            None: SensorDeviceInfo(
+                name="ATC_8D18B2 (A4:C1:38:8D:18:B2)",
+                manufacturer="ATC",
+                model="ATC sensor",
+                sw_version="ATC (pvvx)",
+                hw_version=None,
+            )
+        },
+        entity_descriptions={
+            KEY_TEMPERATURE: SensorDescription(
+                device_key=KEY_TEMPERATURE,
+                device_class=DeviceClass.TEMPERATURE,
+                native_unit_of_measurement=Units.TEMP_CELSIUS,
+            ),
+            KEY_HUMIDITY: SensorDescription(
+                device_key=KEY_HUMIDITY,
+                device_class=DeviceClass.HUMIDITY,
+                native_unit_of_measurement=Units.PERCENTAGE,
+            ),
+            KEY_BATTERY: SensorDescription(
+                device_key=KEY_BATTERY,
+                device_class=DeviceClass.BATTERY,
+                native_unit_of_measurement=Units.PERCENTAGE,
+            ),
+            KEY_VOLTAGE: SensorDescription(
+                device_key=KEY_VOLTAGE,
+                device_class=DeviceClass.VOLTAGE,
+                native_unit_of_measurement=Units.ELECTRIC_POTENTIAL_VOLT,
+            ),
+            KEY_SIGNAL_STRENGTH: SensorDescription(
+                device_key=KEY_SIGNAL_STRENGTH,
+                device_class=DeviceClass.SIGNAL_STRENGTH,
+                native_unit_of_measurement=Units.SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+            ),
+        },
+        entity_values={
+            KEY_TEMPERATURE: SensorValue(
+                device_key=KEY_TEMPERATURE, name="Temperature", native_value=26.49
+            ),
+            KEY_HUMIDITY: SensorValue(
+                device_key=KEY_HUMIDITY, name="Humidity", native_value=50.37
+            ),
+            KEY_BATTERY: SensorValue(
+                device_key=KEY_BATTERY, name="Battery", native_value=31
+            ),
+            KEY_VOLTAGE: SensorValue(
+                device_key=KEY_VOLTAGE, name="Voltage", native_value=2.486
+            ),
+            KEY_SIGNAL_STRENGTH: SensorValue(
+                name="Signal Strength", device_key=KEY_SIGNAL_STRENGTH, native_value=-60
+            ),
+        },
+    )
+
+
+def test_atc_pvvx_with_encryption(caplog):
+    """Test ATC parser for LYWSD03MMC with pvvx firmware with encryption."""
+    bindkey = "b9ea895fac7eea6d30532432a516f3a3"
+    data_string = b'\x11\xd6\x03\xfb\xfa{m\xfb\x1e&\xfd'
+    advertisement = bytes_to_service_info(data_string, local_name="ATC_8D18B2", address="A4:C1:38:8D:18:B2")
+
+    device = ATCBluetoothDeviceData(bindkey=bytes.fromhex(bindkey))
+    assert device.update(advertisement) == SensorUpdate(
+        title="ATC_8D18B2 (A4:C1:38:8D:18:B2)",
+        devices={
+            None: SensorDeviceInfo(
+                name="ATC_8D18B2 (A4:C1:38:8D:18:B2)",
+                manufacturer="ATC",
+                model="ATC sensor",
+                sw_version="ATC (pvvx encrypted)",
+                hw_version=None,
+            )
+        },
+        entity_descriptions={
+            KEY_TEMPERATURE: SensorDescription(
+                device_key=KEY_TEMPERATURE,
+                device_class=DeviceClass.TEMPERATURE,
+                native_unit_of_measurement=Units.TEMP_CELSIUS,
+            ),
+            KEY_HUMIDITY: SensorDescription(
+                device_key=KEY_HUMIDITY,
+                device_class=DeviceClass.HUMIDITY,
+                native_unit_of_measurement=Units.PERCENTAGE,
+            ),
+            KEY_BATTERY: SensorDescription(
+                device_key=KEY_BATTERY,
+                device_class=DeviceClass.BATTERY,
+                native_unit_of_measurement=Units.PERCENTAGE,
+            ),
+            KEY_SIGNAL_STRENGTH: SensorDescription(
+                device_key=KEY_SIGNAL_STRENGTH,
+                device_class=DeviceClass.SIGNAL_STRENGTH,
+                native_unit_of_measurement=Units.SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+            ),
+        },
+        entity_values={
+            KEY_TEMPERATURE: SensorValue(
+                device_key=KEY_TEMPERATURE, name="Temperature", native_value=23.45
+            ),
+            KEY_HUMIDITY: SensorValue(
+                device_key=KEY_HUMIDITY, name="Humidity", native_value=41.73
+            ),
+            KEY_BATTERY: SensorValue(
+                device_key=KEY_BATTERY, name="Battery", native_value=61
+            ),
+            KEY_SIGNAL_STRENGTH: SensorValue(
+                name="Signal Strength", device_key=KEY_SIGNAL_STRENGTH, native_value=-60
+            ),
+        },
+    )
+
+
+def test_atc1441_with_encryption(caplog):
+    """Test ATC parser for LYWSD03MMC with atc1441 firmware with encryption."""
+    bindkey = "b9ea895fac7eea6d30532432a516f3a3"
+    data_string = b'X\xe9\xe6Ue\x81\xb3\xf9'
+    advertisement = bytes_to_service_info(data_string, local_name="ATC_8D18B2", address="A4:C1:38:8D:18:B2")
+
+    device = ATCBluetoothDeviceData(bindkey=bytes.fromhex(bindkey))
+    assert device.update(advertisement) == SensorUpdate(
+        title="ATC_8D18B2 (A4:C1:38:8D:18:B2)",
+        devices={
+            None: SensorDeviceInfo(
+                name="ATC_8D18B2 (A4:C1:38:8D:18:B2)",
+                manufacturer="ATC",
+                model="ATC sensor",
+                sw_version="ATC (atc1441 encrypted)",
+                hw_version=None,
+            )
+        },
+        entity_descriptions={
+            KEY_TEMPERATURE: SensorDescription(
+                device_key=KEY_TEMPERATURE,
+                device_class=DeviceClass.TEMPERATURE,
+                native_unit_of_measurement=Units.TEMP_CELSIUS,
+            ),
+            KEY_HUMIDITY: SensorDescription(
+                device_key=KEY_HUMIDITY,
+                device_class=DeviceClass.HUMIDITY,
+                native_unit_of_measurement=Units.PERCENTAGE,
+            ),
+            KEY_BATTERY: SensorDescription(
+                device_key=KEY_BATTERY,
+                device_class=DeviceClass.BATTERY,
+                native_unit_of_measurement=Units.PERCENTAGE,
+            ),
+            KEY_SIGNAL_STRENGTH: SensorDescription(
+                device_key=KEY_SIGNAL_STRENGTH,
+                device_class=DeviceClass.SIGNAL_STRENGTH,
+                native_unit_of_measurement=Units.SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+            ),
+        },
+        entity_values={
+            KEY_TEMPERATURE: SensorValue(
+                device_key=KEY_TEMPERATURE, name="Temperature", native_value=26.5
+            ),
+            KEY_HUMIDITY: SensorValue(
+                device_key=KEY_HUMIDITY, name="Humidity", native_value=47.0
+            ),
+            KEY_BATTERY: SensorValue(
+                device_key=KEY_BATTERY, name="Battery", native_value=100
+            ),
+            KEY_SIGNAL_STRENGTH: SensorValue(
+                name="Signal Strength", device_key=KEY_SIGNAL_STRENGTH, native_value=-60
+            ),
+        },
+    )
